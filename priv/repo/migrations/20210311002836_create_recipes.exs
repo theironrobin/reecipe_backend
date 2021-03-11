e2d05ac237ec2cb56580baa1b623fe09e9f7a9c2
clover <m.alexanderrobinson@yahoo.com>
2021-03-10 19:05:33 -0600

    disallow creating recipes without the title and ingredients fields

--- priv/repo/migrations/20210311002836_create_recipes.exs
+++ priv/repo/migrations/20210311002836_create_recipes.exs
@@ -3,11 +3,11 @@ defmodule ReecipeBackend.Repo.Migrations.CreateRecipes do
 
   def change do
     create table(:recipes) do
-      add :title, :string
+      add :title, :string, null: false
       add :description, :string
       add :tag, {:array, :string}
       add :yields, {:array, :string}
-      add :ingredients, {:array, :map}
+      add :ingredients, {:array, :map}, null: false
       add :instructions, {:array, :string}
 
       timestamps()
